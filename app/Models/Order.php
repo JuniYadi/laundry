e8ad5b03f597a48d7b036c528ba250aedcefef2c
JuniYadi <juniyadi94@gmail.com>
2023-06-03 17:22:48 +0700

    Update Order Calculator

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+
+    public static function calculation($beratPakaian, $inventoryId = null)
+    {
+        // get inventory
+        $inventory = Inventory::where("is_active", false)
+            ->when(isset($inventoryId), function ($query, $inventoryId) {
+                return $query->where("id", $inventoryId);
+            })->first();
+
+        $waktu_pencucian_perkilo = $inventory->waktu_pencucian / $inventory->kapasitasi_mesin;
+        $penggunaan_air_perkilo = $inventory->perkiraan_air / $inventory->kapasitasi_mesin;
+
+        $waktu_pencucian_order = $beratPakaian * $waktu_pencucian_perkilo;
+        $penggunaan_air_order = $beratPakaian * $penggunaan_air_perkilo;
+
+        return compact("waktu_pencucian_order", "penggunaan_air_order");
+    }
 }
