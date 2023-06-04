06f77b7ae70091b3d56c24ec8645ca62204de83f
JuniYadi <juniyadi94@gmail.com>
2023-06-04 15:08:40 +0700

    Update Whatsapp Notification

--- app/Http/Controllers/UserOrderController.php
+++ app/Http/Controllers/UserOrderController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Order;
+use Illuminate\Support\Facades\Storage;
+use KrmPesan\ClientV3;
 
 class UserOrderController extends Controller
 {
@@ -14,6 +16,28 @@ class UserOrderController extends Controller
             "paketLaundry" => "required|exists:packages,id",
             "nomorTelpon" => "required|numeric"
         ]);
+
+        $phone = $request->input('nomorTelpon');
+
+        $wa = new ClientV3([
+            "tokenFile" => storage_path('app')
+        ]);
+
+        // $wa->sendMessageTemplateText(
+        //     $phone,
+        //     'info_order_laundry',
+        //     'id',
+        //     ['12345', env('APP_URL') . "/order/1234"],
+        // );
+
+        $wa->sendMessageTemplateText(
+            $phone,
+            'info_order_laundry_done',
+            'id',
+            ['12345', 'Selesai dan Sudah Bisa Di Ambil', env('APP_URL') . "/order/1234"],
+        );
+
+
 
         $calculator = Order::calculation($request->input('beratPakaian'));
 
